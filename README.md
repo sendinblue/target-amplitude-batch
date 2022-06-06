46e416bcbca7f0e7292583ee4fdccb72a0aad717
HichemELB <hichem@sendinblue.com>
2022-06-06 18:24:53 +0200

    release first version

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
-# target-amplitude
+# target-amplitude-batch
 
-This is a [Singer](https://singer.io) target that reads JSON-formatted data and send it to Amplitude Batch API
+This is a [Singer](https://singer.io) target that reads JSON-formatted data pulled from BigQuery and send it to Amplitude Batch API
 following the [Singer spec](https://github.com/singer-io/getting-started/blob/master/SPEC.md).
 
 
--- setup.py
+++ setup.py
@@ -1,23 +1,24 @@
 #!/usr/bin/env python
 from setuptools import setup
 
+VERSION = "1.0.0"
+
 setup(
-    name="target-amplitude",
-    version="0.1.0",
+    name="target-amplitude-batch",
+    version=VERSION,
     description="Singer.io target for loading data to Amplitude Batch / Identify APIs",
     author="HichemELB",
     url="http://singer.io",
     classifiers=["Programming Language :: Python :: 3 :: Only"],
-    py_modules=["target_amplitude"],
+    py_modules=["target_amplitude_batch"],
     install_requires=[
-        "singer-python==5.8.0",
+        "singer-python==5.12.2",
         "adjust-precision-for-schema==0.3.3",
         "jsonschema==2.6.0",
-        "singer==0.1.1",
         "amplitude-analytics==0.3.0",
     ],
     entry_points="""
     [console_scripts]
-    target-amplitude=target_amplitude:main
+    target-amplitude-batch=target_amplitude_batch:main
     """,
 )
